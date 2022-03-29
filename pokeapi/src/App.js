f34473ae50554f63afda8f70ecf2ac32f9b7c9c7
Verified ✔️ <ryesan58@gmail.com>
2022-03-29 03:44:04 -0400

    Pokemon api
    using useeffect to create a list from the api and push into the page by mapping all names into a div !

---
+++ pokeapi/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pokemon from './components/poke';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokemon/>
+    </div>
+  );
+}
+
+export default App;
